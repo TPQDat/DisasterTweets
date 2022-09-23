ca98c7bda4c78cd839c726462cced0cfc12ae80f
TranDat <CS.TranDat@gmail.com>
2022-09-23 15:22:45 +0700

    [FIX] code

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ class CFG:
     max_length = 256
     batch_size = 8
     hidden_prob_drop = 0.1
-    device = torch.device('cuda' if torch.cuda.is_availabel() else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     threshold = 0.5
     lr = 3e-5
     num_epochs = 15
+    seed = 42
     pass
